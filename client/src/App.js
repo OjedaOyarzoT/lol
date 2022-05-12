bd1f7d9e89e0ff0357da69af6eff4dd28822edf9
Tamara123 <tojeda2020@alu.uct.cl>
2022-05-11 21:27:38 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import {useState} from 'react';
-import axios from 'axios';
+//import {useState} from 'react';
+//import axios from 'axios';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PagPrincipal from './PagPrincipal';
+//import React from 'react';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {  BrowserRouter as Router,  Routes, Route} from "react-router-dom";
+import PagPrincipal from "./PagPrincipal";
 import Jugadores from './Jugadores';
+
 
 function App() {
 
     
   return (
+         
     <Router>
-    <div className="App">
-      <div className="contenido">
-        <Switch>
-         <Route path={'/'} component={PagPrincipal} />
-         <Route path={'/Jugadores'} component={Jugadores}/>
-        </Switch>
-      </div>   
-      </div>
-      </Router>
+      <Routes>
+        <Route path='/' element={<PagPrincipal/>} />
+        <Route path='/jugador' element={<Jugadores/>} />
+      </Routes>
+    </Router>
+  
+     
   );
 }
 export default App;
