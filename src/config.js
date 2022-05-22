b01a1ff6abe23e2cfc95df40e538b63f028ed22c
Eduardo <eniripil2017@alu.uct.cl>
2022-05-21 20:11:16 -0400

    .

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
-    API_KEY : process.env.API_KEY || 'RGAPI-49e23c9b-fcac-48a0-9648-6ecf7274dfdb',
-=======
-    API_KEY : process.env.API_KEY || 'RGAPI-9f1efeb4-4b4c-4b06-bff1-cb747c463b83',
->>>>>>> c9c8583ec4dcebad0db0e61ef392cf3600663241
+    API_KEY : process.env.API_KEY || 'RGAPI-adbc93a3-522e-442f-9e4d-2784d74b1c2e',
   }
